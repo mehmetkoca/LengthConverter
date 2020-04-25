d50b4ac70c44c7a50b797bef1c642823eecb36dd
Mehmet Koca <mehmetkoca@outlook.com>
2020-04-25 20:47:29 +0300

    Add main length selection view

--- LengthConverter/ContentView.swift
+++ LengthConverter/ContentView.swift
@@ -8,9 +8,59 @@
 
 import SwiftUI
 
+enum Length: String, CaseIterable {
+    
+    case meter = "Meter"
+    case kilometer = "Kilometer"
+    case feet = "Feet"
+    case miles = "Miles"
+    
+    var mainLengthSymbol: String {
+        switch self {
+        case .meter:
+            return "m"
+        case .kilometer:
+            return "km"
+        case .feet:
+            return "ft"
+        case .miles:
+            return "mi"
+        }
+    }
+}
+
 struct ContentView: View {
+    
+    private let lengts = Length.allCases
+    @State private var mainLengthIndex = 0
+    @State private var mainLengthValue = ""
+    @State private var destinationLengthIndex = 0
+    
     var body: some View {
-        Text("Hello, World!")
+        NavigationView {
+            Form {
+                Section(header: Text("Select main length")) {
+                    Picker("Main Length", selection: $mainLengthIndex) {
+                        ForEach(0..<Length.allCases.count) {
+                            Text("\(self.lengts[$0].rawValue)")
+                        }
+                    }.pickerStyle(SegmentedPickerStyle())
+                    
+                    TextField("\(mainLengthResultValue)", text: $mainLengthValue)
+                        .keyboardType(.decimalPad)
+                }
+            }
+            .navigationBarTitle("Length Converter")
+        }
+    }
+}
+
+// MARK: - Computed Properties
+
+extension ContentView {
+
+    var mainLengthResultValue: String {
+        return "\(Double(mainLengthValue) ?? 0.0) \(lengts[mainLengthIndex].mainLengthSymbol)"
     }
 }
 
